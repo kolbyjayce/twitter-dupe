fe342c18a4e7d0cdb65986acbd65880d7fb97c70
Kolby Christiansen <kolbyjayce1@gmail.com>
2023-08-12 21:16:25 -0600

    starting

---
+++ components/Modal.tsx
@@ -0,0 +1,18 @@
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+}
+
+const Modal = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Modal;
